e8ab55610cb21466708048b361a2eb3254fac520
pejko <aleksandarpejkovic@hotmail.com>
2023-11-10 18:43:31 +0100

    first commit

---
+++ src/main/java/com/tasks_with_high_level_complexity/healthcare/HealthcareApplication.java
@@ -0,0 +1,13 @@
+package com.tasks_with_high_level_complexity.healthcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthcareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthcareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tasks_with_high_level_complexity/healthcare/HealthcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tasks_with_high_level_complexity.healthcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
